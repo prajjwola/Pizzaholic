6669c60afd028993f20537029d05908027be67ac
prajjwola karki <prajjwola74@gmail.com>
2024-07-11 06:39:44 +0545

    udpated

---
+++ client/src/app/page.js
@@ -0,0 +1,11 @@
+import Header from "@/components/layout/header";
+import Hero from "@/components/layout/hero";
+
+export default function Home() {
+  return (
+    <>
+  <Header/>
+  <Hero/>
+    </>
+  )
+}
